6bc322b4a269641fc9e36099072c06eb6ad29a99
LeChiffr3 <matthieu.pommier@gmail.com>
2016-02-15 14:13:42 +0100

    Create penney-monte-carlo.r
    # Monte-Carlo simulation to solve the Penney's game, as described here : http://blog.gtorangebuilder.com/2016/02/gto-brainteaser-10-flipping-coins.html

---
+++ penney-monte-carlo.r
@@ -0,0 +1,34 @@
+#A Monte-Carlo simulation to solve the Penney's game, as described here : http://blog.gtorangebuilder.com/2016/02/gto-brainteaser-10-flipping-coins.html
+
+library(ggplot2)
+
+#The game, it stops when the last three flips are "Head Head Tail" (0,0,1) or "Head Tail Tail" (0,1,1). It returns the last three flips.
+game <- function(){
+  #head = 0, tail = 1
+  a <- sample(c(0,1),1)
+  b <- sample(c(0,1),1)
+  c <- sample(c(0,1),1)
+  
+  while(!all(c(a,b,c) == c(0,0,1)) & !all(c(a,b,c) == c(0,1,1)))
+    {a <- b
+    b <- c
+    c <- sample(c(0,1),1)}
+return(paste(a, b, c))}
+
+
+#The simulation, it plays n games and return the profit
+simulation <- function(n){
+  simul <- replicate(n, game())
+  simul <- simul == '0 1 1'
+  simul[simul] <- 205
+  simul[!simul] <- -100
+  return(sum(simul))}
+
+#The monte carlot simulation, it plays a number of simulations of n games and save the profits in a vector.
+monte <- replicate(10000, simulation(100))
+
+#The sample mean that estimate the Expected Value
+mean(monte)/100
+
+#The sample distribution, to get a sense of the variance
+ggplot(data.frame(x = monte), aes(x=x)) + geom_histogram()
